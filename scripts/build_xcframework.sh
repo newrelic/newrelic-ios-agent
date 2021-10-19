6dd92baa06083355b4adee8daa24849027ecd8de
Steve Malsam <smalsam@newrelic.com>
2021-10-19 13:00:21 -0700

    Get all files in the BCSymbolMaps directory

--- scripts/build_xcframework.sh
+++ scripts/build_xcframework.sh
@@ -5,10 +5,11 @@
 
 function getiOSBCFiles() {
 	IPHONE_BCSYMBOLMAP_COMMANDS=""
-	IPHONE_BCSYMBOLMAP_PATHS="build/iphoneos/iOS.xcarchive/BCSymbolMaps/"
+	IPHONE_BCSYMBOLMAP_PATHS="build/iphoneos/iOS.xcarchive/BCSymbolMaps/*"
 
-	for path in "${IPHONE_BSCYMBOLMAP_PATHS[@]}"; do
-		IPHONE_BCSYMBOLMAP_COMMANDS="${IPHONE_BCSYMBOLMAP_COMMANDS} -debug-symbols $path "
+	for path in $IPHONE_BSCYMBOLMAP_PATHS; do
+		fullPath=$(pwd)/$path
+		IPHONE_BCSYMBOLMAP_COMMANDS="${IPHONE_BCSYMBOLMAP_COMMANDS} -debug-symbols $fullPath "
 	done
 
 	echo $IPHONE_BCSYMBOLMAP_COMMANDS
@@ -16,24 +17,27 @@ function getiOSBCFiles() {
 
 function gettvOSBCFiles() {
 	TVOS_BCSYMBOLMAP_COMMANDS=""
-	TVOS_BCSYMBOLMAP_PATHS="build/appletvos/tvOS.xcarchive/BCSymbolMaps/"
+	TVOS_BCSYMBOLMAP_PATHS="build/appletvos/tvOS.xcarchive/BCSymbolMaps/*"
 
-	for path in "${IPHONE_BCSYMBOLMAP_PATHS[@]}"; do
-		TVOS_BCSYMBOLMAP_COMMANDS="${TVOS_BCSYMBOLMAP_COMMANDS} -debug-symbols $path"
+	for path in $TVOS_BCSYMBOLMAP_PATHS; do
+		fullPath=$(pwd)/$path
+		TVOS_BCSYMBOLMAP_COMMANDS="${TVOS_BCSYMBOLMAP_COMMANDS} -debug-symbols $fullPath"
 	done
 
 	echo $TVOS_BCSYMBOLMAP_COMMANDS
 }
 
+getiOSBCFiles
+gettvOSBCFiles
 xcodebuild -create-xcframework \
 	-framework build/iphoneos/NewRelic.framework \
 		-debug-symbols /Users/jenkins/workspace/Agent-Generate-XCFramework/build/iphoneos/iOS.xcarchive/dSYMs/NewRelic.framework.dSYM \
-		$(getiOSBCFiles) \
+		$IPHONE_BCSYMBOLMAP_COMMANDS \
 	-framework build/iphonesimulator/NewRelic.framework \
 	-framework build/appletvsimulator/NewRelic.framework/ \
 	-framework build/appletvos/NewRelic.framework \
 		-debug-symbols /Users/jenkins/workspace/Agent-Generate-XCFramework/build/appletvos/tvOS.xcarchive/dSYMs/NewRelic.framework.dSYM \
-		$(gettvOSBCFiles) \
+		$TVOS_BCSYMBOLMAP_COMMANDS \
 	-framework build/macosx/NewRelic.framework 	\
 		-debug-symbols /Users/jenkins/workspace/Agent-Generate-XCFramework/build/macosx/macosx.xcarchive/dSYMs/NewRelic.framework.dSYM \
 	-output build/NewRelic.xcframework
