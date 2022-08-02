57a5f8680524783cd938074124f4d347d5594d9a
Chris Dillard <cdillard@newrelic.com>
2022-08-02 14:36:30 -0600

    NR-38977: Fix memoryvital MB amount to be correct using phys_footprint (#110)

--- Agent/Utilities/NRMAMemoryVitals.m
+++ Agent/Utilities/NRMAMemoryVitals.m
@@ -18,9 +18,7 @@ static NSString *__NRMAMemoryVitalsLock = @"NRMAMemoryVitalsLock";
 static double __lastCachedMillis;
 static double __lastCachedMemoryUsage;
 
-
-
-//http://stackoverflow.com/questions/787160/programmatically-retrieve-memory-usage-on-iphone
+// http://www.samirchen.com/ios-app-memory-usage/
 + (double) memoryUseInMegabytes {
 
     @synchronized(__NRMAMemoryVitalsLock) {
@@ -30,19 +28,17 @@ static double __lastCachedMemoryUsage;
         }
         __lastCachedMillis = currentTime;
 
-        struct mach_task_basic_info info;
-        mach_msg_type_number_t size = MACH_TASK_BASIC_INFO_COUNT;
-        kern_return_t kerr = task_info(mach_task_self(),
-                                       MACH_TASK_BASIC_INFO,
-                                       (task_info_t)&info,
-                                       &size);
-        if( kerr == KERN_SUCCESS ) {
-            __lastCachedMemoryUsage = (float)info.resident_size / BYTES_PER_MB;
+        task_vm_info_data_t vmInfo;
+        mach_msg_type_number_t count = TASK_VM_INFO_COUNT;
+        kern_return_t kernelReturn = task_info(mach_task_self(), TASK_VM_INFO, (task_info_t) &vmInfo, &count);
+        if(kernelReturn == KERN_SUCCESS) {
+            __lastCachedMemoryUsage = (float)vmInfo.phys_footprint / BYTES_PER_MB;
             [[NSNotificationCenter defaultCenter] postNotificationName:kNRMemoryUsageDidChangeNotification
                                                                 object:@(__lastCachedMemoryUsage)];
+
             return __lastCachedMemoryUsage;
         } else {
-            NRLOG_ERROR(@"Error with task_info(): %s", mach_error_string(kerr));
+            NRLOG_ERROR(@"Error with task_info(): %s", mach_error_string(kernelReturn));
             return 0;
         }
     }
