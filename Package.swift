f12fd6c738b0823a39b13a78af767f6cabcff24d
cdillard-NewRelic <cdillard-NewRelic@users.noreply.github.com>
2023-01-06 00:21:30 +0000

    [7.4.1-dev.55] Staging SPM

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "NewRelic",
-                      url: "https://download.newrelic.com/ios-v5/NewRelic_XCFramework_Agent_7.4.1-dev.54.zip",
-                      checksum: "f2f506deb05dfd30be7042e964594e3f678f2fece43616c74d933cc962464478")
+                      url: "https://download.newrelic.com/ios-v5/NewRelic_XCFramework_Agent_7.4.1-dev.55.zip",
+                      checksum: "9a5d2dedbf1cc3c7de4507f0b497b5411f4ac9eca889ce5f01f35060a25561a7")
     ]
 )
 
