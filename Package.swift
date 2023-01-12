eeea26f75ed83dd917619716246d8316f3821922
cdillard-NewRelic <cdillard-NewRelic@users.noreply.github.com>
2023-01-12 19:38:40 +0000

    [7.4.1-rc.73] Staging Release

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "NewRelic",
-                      url: "https://download.newrelic.com/ios-v5/NewRelic_XCFramework_Agent_7.4.1-rc.68.zip",
-                      checksum: "b6d3c1a2a72a33da254ba85bd4808665e59d9111aabf26343c682961dda8c958")
+                      url: "https://download.newrelic.com/ios-v5/NewRelic_XCFramework_Agent_7.4.1-rc.73.zip",
+                      checksum: "ea19909f69b620c1345d585eb46063391618ddbba6593c2e3e3a824787de2a95")
     ]
 )
 
