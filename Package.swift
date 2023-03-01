d6ff819c7dbd70eb76c81e1bb07d71fbfb0d04c9
cdillard-NewRelic <cdillard-NewRelic@users.noreply.github.com>
2023-03-01 21:17:10 +0000

    [create-pull-request] automated change

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "NewRelic",
-                      url: "https://download.newrelic.com/ios-v5/NewRelic_XCFramework_Agent_7.4.3-rc.192.zip",
-                      checksum: "6cfa1c915a675a0cc91b4dd3cf560efe1f5baea24501f6f3773e63f3d8c0cd4c")
+                      url: "https://download.newrelic.com/ios_agent/NewRelic_XCFramework_Agent_7.4.3.zip",
+                      checksum: "deadbeef")
     ]
 )
 
