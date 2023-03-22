b637638b893933af8e55188ee28e5daa0af93c81
Mike Bruin <mbruin@newrelic.com>
2023-03-22 14:14:54 -0400

    Got rid of the second file for webkit tests and combined them back into one

--- Tests/Unit-Tests/NewRelicAgentTests/Uncategorized/NRMAWKNavigationDelegateBaseTest.m
+++ Tests/Unit-Tests/NewRelicAgentTests/Uncategorized/NRMAWKNavigationDelegateBaseTest.m
@@ -13,6 +13,9 @@
 #import <WebKit/WebKit.h>
 #import "NRMAWKFakeNavigationAction.h"
 
+@interface NRMAWKNavigationDelegateWithDelegateFunctions : NSObject <WKNavigationDelegate>
+@end
+
 @interface NRWKNavigationDelegateBase ()
 - (instancetype) initWithOriginalDelegate:(NSObject<WKNavigationDelegate>* __nullable __weak)delegate;
 + (NSURL*) navigationURL:(WKNavigation*) nav;
@@ -28,6 +31,9 @@
 @property(strong) NRMAWKWebViewNavigationDelegate* navBase;
 @property(strong) WKWebView* webView;
 
+@property(strong) NRMAWKWebViewNavigationDelegate* navBaseWithDelegateFunction;
+@property(strong) NRMAWKNavigationDelegateWithDelegateFunctions* delegateFunctions;
+@property(strong) WKWebView* webViewWithDelegateFunction;
 
 @end
 
@@ -41,6 +47,11 @@
     self.timer = [[NRTimer alloc] init];
     self.webView = [[WKWebView alloc] init];
     self.webView.navigationDelegate = _navBase;
+    
+    self.delegateFunctions = [[NRMAWKNavigationDelegateWithDelegateFunctions alloc] init];
+    self.navBaseWithDelegateFunction = [[NRMAWKWebViewNavigationDelegate alloc] initWithOriginalDelegate:_delegateFunctions];
+    self.webViewWithDelegateFunction = [[WKWebView alloc] init];
+    self.webViewWithDelegateFunction.navigationDelegate = _navBaseWithDelegateFunction;
 }
 
 - (void)tearDown {
@@ -107,6 +118,74 @@
     }];;
     
     XCTAssertEqual(testResponse.receivedPolicy, WKNavigationResponsePolicyAllow);
+}
+
+- (void) testDecidePolicyForNavigationActionWithDelegateFunctions {
+    NSURLRequest* url = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"localhost"]];
+    
+    NRMAWKFakeNavigationAction *testAction = [[NRMAWKFakeNavigationAction alloc] initWith:url];
+    
+    [self.webViewWithDelegateFunction.navigationDelegate webView:self.webViewWithDelegateFunction decidePolicyForNavigationAction:testAction decisionHandler:^(WKNavigationActionPolicy policy){
+        [testAction decisionHandler:policy];
+    }];
+    
+    XCTAssertEqual(testAction.receivedPolicy, WKNavigationActionPolicyAllow);
+    
+    if (@available(iOS 13.0, *)) {
+        [self.webViewWithDelegateFunction.navigationDelegate webView:self.webViewWithDelegateFunction decidePolicyForNavigationAction:testAction preferences:[[WKWebpagePreferences alloc] init] decisionHandler:^(WKNavigationActionPolicy policy, WKWebpagePreferences* preference){
+            [testAction decisionHandler:policy];
+        }];
+        XCTAssertEqual(testAction.receivedPolicy, WKNavigationActionPolicyAllow);
+    }
+}
+
+- (void) testDidReceiveAuthenticationChallengeWithDelegateFunctions {
+    NSURLRequest* url = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"localhost"]];
+    
+    NRMAWKFakeURLAuthenticationChallenge *testChallenge = [[NRMAWKFakeURLAuthenticationChallenge alloc] initWith:url];
+    
+    [self.webViewWithDelegateFunction.navigationDelegate webView:self.webViewWithDelegateFunction didReceiveAuthenticationChallenge:[[NSURLAuthenticationChallenge alloc] init] completionHandler:^(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential *credential){
+        [testChallenge completionHandler:disposition withCredential:credential];
+    }];
+    
+    XCTAssertNil(testChallenge.credential);
+    XCTAssertEqual(testChallenge.authenticationChallengeDisposition, NSURLSessionAuthChallengePerformDefaultHandling);
+}
+
+- (void) testDecidePolicyForNavigationResponseWithDelegateFunctions {
+    NSURLRequest* url = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"localhost"]];
+    
+    NRMAWKFakeNavigationResponse *testResponse = [[NRMAWKFakeNavigationResponse alloc] initWith:url];
+    
+    [self.webViewWithDelegateFunction.navigationDelegate webView:self.webViewWithDelegateFunction decidePolicyForNavigationResponse:testResponse decisionHandler:^(WKNavigationResponsePolicy policy){
+        [testResponse decisionHandler:policy];
+    }];;
+    
+    XCTAssertEqual(testResponse.receivedPolicy, WKNavigationResponsePolicyAllow);
+}
+
+@end
+
+@implementation NRMAWKNavigationDelegateWithDelegateFunctions
+#pragma mark Delegate Functions
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler
+{
+    decisionHandler(WKNavigationActionPolicyAllow);
+}
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction preferences:(WKWebpagePreferences *)preferences decisionHandler:(void (^)(WKNavigationActionPolicy, WKWebpagePreferences *))decisionHandler API_AVAILABLE(ios(13.0))
+{
+    decisionHandler(WKNavigationActionPolicyAllow, preferences);
+}
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler
+{
+    decisionHandler(WKNavigationResponsePolicyAllow);
+}
+
+- (void)webView:(WKWebView *)webView didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential *credential))completionHandler {
+    completionHandler(NSURLSessionAuthChallengePerformDefaultHandling, nil);
 }
 
 @end
