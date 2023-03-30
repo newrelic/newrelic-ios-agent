8c16f478523fb33958fd6b567623b219e6350a1f
Chris Dillard <cdillard@newrelic.com>
2023-03-30 11:57:00 -0600

    fix rmved comment, add additional test for payload case

--- Agent/Instrumentation/NSURLSession/NRMAURLSessionTaskOverride.m
+++ Agent/Instrumentation/NSURLSession/NRMAURLSessionTaskOverride.m
@@ -96,9 +96,11 @@ void NRMAOverride__resume(id self, SEL _cmd)
 {
     if (((NSURLSessionTask*)self).state == NSURLSessionTaskStateSuspended) {
 
-        //and since we are only instrumenting NSURLSessionUploadTask and
-        //NSURLSessionDataTask we only need to start a new timer on this transmission
-        //since those two restart if they are suspended.
+        // The only state resume will start a task is from Suspended.
+        // and since we are only instrumenting NSURLSessionUploadTask and
+        // NSURLSessionDataTask we only need to start a new timer on this transmission
+        // since those two restart if they are suspended.
+
         NRMA__setTimerForSessionTask(self, [NRTimer new]);
     }
     //call original method
