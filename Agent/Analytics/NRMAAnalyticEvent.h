9322781726db19970c659dab0aea89db6dc79fb9
Steve Malsam <smalsam@newrelic.com>
2023-06-08 10:42:46 -0700

    Added stub for event manager and base event
    NRMAAnalytics will serve as the Analytics Controller. It currently has the existing implementation #ifdef out, causing tests to fail. This will eventually be replaced with a feature flag.

---
+++ Agent/Analytics/NRMAAnalyticEvent.h
@@ -0,0 +1,19 @@
+//
+//  NRMAAnalyticEvent.h
+//  Agent
+//
+//  Created by Steve Malsam on 6/8/23.
+//  Copyright © 2023 New Relic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "NRMAAnalyticEventProtocol.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NRMAAnalyticEvent : NSObject <NRMAAnalyticEventProtocol>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Agent/Analytics/NRMAEventManager.h
@@ -0,0 +1,18 @@
+//
+//  NRMAEventManager.h
+//  Agent
+//
+//  Created by Steve Malsam on 6/7/23.
+//  Copyright © 2023 New Relic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NRMAEventManager : NSObject
+
+- (instancetype)init;
+@end
+
+NS_ASSUME_NONNULL_END
