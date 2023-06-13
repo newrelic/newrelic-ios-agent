6db224b9ba62784b191b093dfb08bf4e7926e82c
Steve Malsam <smalsam@newrelic.com>
2023-06-13 09:55:03 -0700

    NR-119407 - Stubbed out EventManager

--- Tests/Unit-Tests/NewRelicAgentTests/Analytics-Tests/TestIntegratedEventManager.m
+++ Tests/Unit-Tests/NewRelicAgentTests/Analytics-Tests/TestIntegratedEventManager.m
@@ -10,6 +10,9 @@
 
 #import "NRMAEventManager.h"
 
+#import "NRMAAnalyticEvent.h"
+
+
 @interface TestIntegratedEventManager : XCTestCase
 @end
 
@@ -25,16 +28,34 @@
     // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
-//- (void)testExample {
-//    // This is an example of a functional test case.
-//    // Use XCTAssert and related functions to verify your tests produce the correct results.
-//}
-//
-//- (void)testPerformanceExample {
-//    // This is an example of a performance test case.
-//    [self measureBlock:^{
-//        // Put the code you want to measure the time of here.
-//    }];
-//}
+- (void)testAddEvent {
+    // Given
+    NRMAAnalyticEvent *testEvent = [NRMAAnalyticEvent new];
+    
+    // When
+    BOOL result = [sut addEvent:testEvent];
+    
+    // Then
+    XCTAssertTrue(result,"Adding the event was not successful");
+}
+
+- (void)testRetrieveEventJSON {
+    // Given
+    NRMAAnalyticEvent *testEvent = [NRMAAnalyticEvent new];
+    NSError *error = nil;
+    
+    // When
+    [sut addEvent:testEvent];
+    NSString *eventJSONString = [sut getEventJSONStringWithError:&error];
+    
+    // Then
+    XCTAssertNotNil(eventJSONString, "Event JSON String not properly created");
+    
+    NSArray *decode = [NSJSONSerialization JSONObjectWithData:[eventJSONString dataUsingEncoding:NSUTF8StringEncoding]
+                                                    options:0
+                                                      error:nil];
+    XCTAssertNotNil(decode[0][@"timestamp"]);
+    XCTAssertNotNil(decode[0][@"timeSinceLoad"]);
+}
 
 @end
