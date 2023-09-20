0362e75cf50f8e315bb82f967c5fd95bef2066a2
Chris Dillard <cdillard@newrelic.com>
2023-09-20 13:24:57 -0600

    Add 3 new simple examples: SPM, Cocoapods, Framework

---
+++ examples/cocoapods/CocoapodsExample/CocoapodsExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CocoapodsExample
+//
+//  Created by Chris Dillard on 9/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
