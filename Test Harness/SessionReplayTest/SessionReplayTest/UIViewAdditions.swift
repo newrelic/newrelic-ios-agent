796bff627e8a42120db8d3ad2dccce0f85205b25
Steve Malsam <smalsam@newrelic.com>
2024-04-12 16:05:06 -0700

    Added Session Replay test view

---
+++ Test Harness/SessionReplayTest/SessionReplayTest/UIViewAdditions.swift
@@ -0,0 +1,40 @@
+//
+//  UIViewAdditions.swift
+//  SessionReplayTest
+//
+//  Created by Steve Malsam on 4/12/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
