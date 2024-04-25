ce2e6c8d0152881ce0559db210151726871d5f50
Chris Dillard <cdillard@newrelic.com>
2024-04-25 12:42:08 -0600

    Temporarily remove LogReporting

--- Agent/FeatureFlags/NRMAFlags.m
+++ Agent/FeatureFlags/NRMAFlags.m
@@ -167,7 +167,9 @@ static NSString* __deviceIdentifierReplacement = NULL;
 }
 
 + (BOOL) shouldEnableLogReporting {
-    return ([NRMAFlags featureFlags] & NRFeatureFlag_LogReporting) != 0;
+    // TODO: LogReporting
+    return NO;
+//    return ([NRMAFlags featureFlags] & NRFeatureFlag_LogReporting) != 0;
 }
 + (BOOL) shouldEnableNewEventSystem {
     return ([NRMAFlags featureFlags] & NRFeatureFlag_NewEventSystem) != 0;
@@ -230,9 +232,10 @@ static NSString* __deviceIdentifierReplacement = NULL;
     if ((flags & NRFeatureFlag_OfflineStorage) == NRFeatureFlag_OfflineStorage) {
         [retArray addObject:@"OfflineStorage"];
     }
-    if ((flags & NRFeatureFlag_LogReporting) == NRFeatureFlag_LogReporting) {
-        [retArray addObject:@"LogReporting"];
-    }
+// TODO: LogReporting
+//    if ((flags & NRFeatureFlag_LogReporting) == NRFeatureFlag_LogReporting) {
+//        [retArray addObject:@"LogReporting"];
+//    }
     if ((flags & NRFeatureFlag_NewEventSystem) == NRFeatureFlag_NewEventSystem) {
         [retArray addObject:@"NewEventSystem"];
     }
