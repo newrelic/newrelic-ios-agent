3a6f5342d5bf5108f2e9e643e316dd19ac375e11
Mike Bruin <mbruin@newrelic.com>
2024-05-07 11:06:33 -0400

    removed unneeded UITesting lines

--- Test Harness/NRTestApp/NRTestApp (watchOS) Watch App/WatchAppDelegate.swift
+++ Test Harness/NRTestApp/NRTestApp (watchOS) Watch App/WatchAppDelegate.swift
@@ -24,32 +24,21 @@ class WatchAppDelegate: NSObject, WKApplicationDelegate {
 
         NewRelic.setMaxEventBufferTime(60)
 
-        if ProcessInfo.processInfo.environment["UITesting"] != nil {
-            if ProcessInfo.processInfo.environment["DeleteConnect"] != nil {
-                clearConnectUserDefaults()
-            }
+        // Generate your own api key to see data get sent to your app's New Relic web services.
+        guard let apiKey = plistHelper.objectFor(key: "NRAPIKey", plist: "NRAPI-Info") as? String else {return}
 
-            NewRelic.start(withApplicationToken: "APP-TOKEN-NRMA",
-                           andCollectorAddress: "localhost:8080",
-                           andCrashCollectorAddress: "localhost:8080")
-        }
-        else {
-            // Generate your own api key to see data get sent to your app's New Relic web services.
-            guard let apiKey = plistHelper.objectFor(key: "NRAPIKey", plist: "NRAPI-Info") as? String else {return}
+        // Changing the collector and crash collector addresses is not necessary to use New Relic production servers.
+        guard let collectorAddress = plistHelper.objectFor(key: "collectorAddress", plist: "NRAPI-Info") as? String, let crashCollectorAddress = plistHelper.objectFor(key: "crashCollectorAddress", plist: "NRAPI-Info") as? String else {return}
 
-            // Changing the collector and crash collector addresses is not necessary to use New Relic production servers.
-            guard let collectorAddress = plistHelper.objectFor(key: "collectorAddress", plist: "NRAPI-Info") as? String, let crashCollectorAddress = plistHelper.objectFor(key: "crashCollectorAddress", plist: "NRAPI-Info") as? String else {return}
-
-            // If the entries for collectorAddress or crashCollectorAddress are empty in NRAPI-Info.plist, start the New Relic agent with default production endpoints.
-            if collectorAddress.isEmpty || crashCollectorAddress.isEmpty {
-                // Start the agent using default endpoints.
-                NewRelic.start(withApplicationToken:apiKey)
-            } else {
-                // Start the agent with custom endpoints.
-                NewRelic.start(withApplicationToken:apiKey,
-                               andCollectorAddress: collectorAddress,
-                               andCrashCollectorAddress: crashCollectorAddress)
-            }
+        // If the entries for collectorAddress or crashCollectorAddress are empty in NRAPI-Info.plist, start the New Relic agent with default production endpoints.
+        if collectorAddress.isEmpty || crashCollectorAddress.isEmpty {
+            // Start the agent using default endpoints.
+            NewRelic.start(withApplicationToken:apiKey)
+        } else {
+            // Start the agent with custom endpoints.
+            NewRelic.start(withApplicationToken:apiKey,
+                           andCollectorAddress: collectorAddress,
+                           andCrashCollectorAddress: crashCollectorAddress)
         }
 
         NewRelic.setMaxEventPoolSize(5000)
@@ -58,13 +47,4 @@ class WatchAppDelegate: NSObject, WKApplicationDelegate {
         NewRelic.logVerbose("NewRelic.start was called.")
     }
 
-}
-
-extension WKApplicationDelegate {
-    func clearConnectUserDefaults() {
-        UserDefaults.standard.removeObject(forKey: "com.newrelic.connectionInformation")
-        UserDefaults.standard.removeObject(forKey: "com.newrelic.harvesterConfiguration")
-        UserDefaults.standard.removeObject(forKey: "com.newrelic.applicationIdentifier")
-        UserDefaults.standard.synchronize()
-    }
 }
