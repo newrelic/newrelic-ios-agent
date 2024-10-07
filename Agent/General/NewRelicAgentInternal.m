9200d2d11e449ac2a4aac53a634d2dfb83766952
Chris Dillard <cdillard@newrelic.com>
2024-10-07 14:17:14 -0600

    NR-296618: deallocInteractionHistoryList fix

--- Agent/General/NewRelicAgentInternal.m
+++ Agent/General/NewRelicAgentInternal.m
@@ -756,6 +756,7 @@ static UIBackgroundTaskIdentifier background_task;
     @try {
 #endif
         [NRMATraceController completeActivityTrace];
+
         [NRMAInteractionHistoryObjCInterface deallocInteractionHistory];
 #ifndef  DISABLE_NRMA_EXCEPTION_WRAPPER
     } @catch (NSException* exception) {
@@ -1045,8 +1046,21 @@ void applicationDidEnterBackgroundCF(void) {
         [NRMAUDIDManager deleteStoredID];
 
         // Stored device data, Metadata and crash file are cleared when crash upload.
-        [NRMAInteractionHistoryObjCInterface deallocInteractionHistory];
+#ifndef  DISABLE_NR_EXCEPTION_WRAPPER
+    @try {
+#endif
+        [NRMATraceController completeActivityTrace];
 
+        // REWRITE
+        [NRMAInteractionHistoryObjCInterface deallocInteractionHistory];
+#ifndef  DISABLE_NRMA_EXCEPTION_WRAPPER
+    } @catch (NSException* exception) {
+        [NRMAExceptionHandler logException:exception
+                                     class:NSStringFromClass([self class])
+                                  selector:NSStringFromSelector(_cmd)];
+    }
+#endif
+        
         // Clear stored user defaults
         [[[NRMAHarvestController harvestController] harvester] clearStoredConnectionInformation];
         [[[NRMAHarvestController harvestController] harvester] clearStoredHarvesterConfiguration];
