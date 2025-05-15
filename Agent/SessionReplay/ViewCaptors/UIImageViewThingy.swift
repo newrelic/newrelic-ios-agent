016cc11f1a24749a14c5a52dade2c283b4a4089c
Mike Bruin <mbruin@newrelic.com>
2025-05-15 16:19:19 -0400

    Adding the ability to have image data in the session replay frame but left it commented out

--- Agent/SessionReplay/ViewCaptors/UIImageViewThingy.swift
+++ Agent/SessionReplay/ViewCaptors/UIImageViewThingy.swift
@@ -11,6 +11,7 @@ import UIKit
 
 class UIImageViewThingy: SessionReplayViewThingy {
     var viewDetails: ViewDetails
+    //var imageData: Data?
     
     var shouldRecordSubviews: Bool {
         true
@@ -20,39 +21,75 @@ class UIImageViewThingy: SessionReplayViewThingy {
     
     init(view: UIImageView, viewDetails: ViewDetails) {
         self.viewDetails = viewDetails
+       // self.imageData = (view.image?.compressImage().pngData() ?? nil)
     }
     
     func cssDescription() -> String {
         let cssSelector = viewDetails.cssSelector
         
-        let imagePlaceholderCSS = "background: rgb(2,0,36);background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(0,212,255,1) 100%);"
-        
-        return "#\(viewDetails.cssSelector) { \(generateBaseCSSStyle()) \(imagePlaceholderCSS) }"
+       // if let _ = imageData {
+         //   return "#\(viewDetails.cssSelector) { \(generateBaseCSSStyle()) }"
+//        } else {
+            let imagePlaceholderCSS = "background: rgb(2,0,36);background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(0,212,255,1) 100%);"
+            return "#\(viewDetails.cssSelector) { \(generateBaseCSSStyle()) \(imagePlaceholderCSS) }"
+//        }
     }
     
     func generateRRWebNode() -> ElementNodeData {
-        return ElementNodeData(id: viewDetails.viewId,
-                                        tagName: .div,
-                                        attributes: ["id":viewDetails.cssSelector],
-                                        childNodes: [])
+//        if let imageData = imageData {
+//            return ElementNodeData(id: viewDetails.viewId,
+//                                   tagName: .image,
+//                                   attributes: ["id":viewDetails.cssSelector,"src":"data:image/png;base64,\(imageData.base64EncodedString())"],
+//                                   childNodes: [])
+//        } else {
+            return ElementNodeData(id: viewDetails.viewId,
+                                   tagName: .div,
+                                   attributes: ["id":viewDetails.cssSelector],
+                                   childNodes: [])
+   //     }
     }
     
     func generateDifference<T: SessionReplayViewThingy>(from other: T) -> [MutationRecord] {
         guard let typedOther = other as? UIImageViewThingy else {
             return []
         }
-        return [RRWebMutationData.AttributeRecord(id: viewDetails.viewId, attributes: generateBaseDifferences(from: typedOther))]
+        var differences = generateBaseDifferences(from: typedOther)
+//        if let imageData = typedOther.imageData {
+//            differences["src"] = "data:image/png;base64,\(imageData.base64EncodedString())"
+//        }
+        return [RRWebMutationData.AttributeRecord(id: viewDetails.viewId, attributes: differences)]
     }
 }
 
 extension UIImageViewThingy: Equatable {
     static func == (lhs: UIImageViewThingy, rhs: UIImageViewThingy) -> Bool {
-        return lhs.viewDetails == rhs.viewDetails
+        return lhs.viewDetails == rhs.viewDetails //&& lhs.imageData?.hashValue == rhs.imageData?.hashValue
     }
 }
 
 extension UIImageViewThingy: Hashable {
     func hash(into hasher: inout Hasher) {
         hasher.combine(viewDetails)
+       // hasher.combine(imageData?.hashValue ?? 0)
+    }
+}
+
+internal extension UIImage {
+    func aspectFittedToHeight(_ newHeight: CGFloat) -> UIImage {
+        let scale = newHeight / self.size.height
+        let newWidth = self.size.width * scale
+        let newSize = CGSize(width: newWidth, height: newHeight)
+        let renderer = UIGraphicsImageRenderer(size: newSize)
+
+        return renderer.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: newSize))
+        }
+    }
+    
+    func compressImage() -> UIImage {
+            let resizedImage = self.aspectFittedToHeight(20)
+            resizedImage.jpegData(compressionQuality: 0.1)
+
+            return resizedImage
     }
 }
