898cc685b87820388aff2b8bc098be6fbcda3d91
Chris Dillard <cdillard@newrelic.com>
2025-05-21 10:40:18 -0600

    Try seventh experimental build

--- Package.swift
+++ Package.swift
@@ -4,23 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "NewRelicAgent",
+    name: "NewRelic",
     platforms: [
         .iOS(.v12), .macOS(.v10_14), .tvOS(.v12), .watchOS(.v10),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "NewRelicAgent",
-            targets: ["NewRelicPackage", "NewRelicAgent"])
+            name: "NewRelic",
+            targets: ["NewRelicPackage", "NewRelic"])
     ],
     targets: [
         .target(
             name: "NewRelicPackage",
             dependencies: []),
         .binaryTarget(
-            name: "NewRelicAgent",
-            url: "https://download.newrelic.com/ios-v5/NewRelic_XCFramework_Agent_7.5.6-dev.28.zip",
-            checksum: "ba6603060e7c5cb31fce5c9d3ad088b22cb0703de5315cc11f60436b420b8017"),
+            name: "NewRelic",
+            url: "https://download.newrelic.com/ios-v5/NewRelic_XCFramework_Agent_7.5.6-dev.30.zip",
+            checksum: "425588fa21f12f916dccb8c59a1f88361542dacefc8d575e4e7f85753bd3f503"),
     ]
 )
