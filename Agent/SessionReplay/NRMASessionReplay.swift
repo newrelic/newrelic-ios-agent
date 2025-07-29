225917e89c98a690a3b15c3d98f00a7680f45e5d
mbruin-NR <118208503+mbruin-NR@users.noreply.github.com>
2025-07-29 13:02:11 -0400

    Adding a null check for the touch capture in clear data (#443)
    * Adding a null check for the touch capture in clear data
    
    * Added the same check to the last location

--- Agent/SessionReplay/NRMASessionReplay.swift
+++ Agent/SessionReplay/NRMASessionReplay.swift
@@ -19,7 +19,7 @@ public class NRMASessionReplay: NSObject {
 
     private let sessionReplayCapture: SessionReplayCapture
     private let sessionReplayFrameProcessor = SessionReplayFrameProcessor()
-    private var sessionReplayTouchCapture: SessionReplayTouchCapture!
+    private var sessionReplayTouchCapture: SessionReplayTouchCapture?
     private let sessionReplayTouchProcessor = TouchEventProcessor()
     private var rawFrames = [SessionReplayFrame]()
     
@@ -62,7 +62,9 @@ public class NRMASessionReplay: NSObject {
     
     public func clearAllData() {
         rawFrames.removeAll()
-        sessionReplayTouchCapture.resetEvents()
+        if let touchCapture = sessionReplayTouchCapture {
+            touchCapture.resetEvents()
+        }
         // should remove frames from file system after processing
 
         // Clear the session replay file after processing
@@ -92,7 +94,10 @@ public class NRMASessionReplay: NSObject {
             let originalSelector = #selector(UIApplication.sendEvent(_:))
 
             let block: @convention(block)(UIApplication, UIEvent) -> Void = { app, event in
-                self.sessionReplayTouchCapture.captureSendEventTouches(event: event)
+                guard let touchCapture = self.sessionReplayTouchCapture else {
+                    return
+                }
+                touchCapture.captureSendEventTouches(event: event)
                 typealias Func = @convention(c)(AnyObject, Selector, UIEvent) -> Void
                 let function = unsafeBitCast(self.NRMAOriginal__sendEvent, to: Func.self)
                 function(app, originalSelector, event)
@@ -207,7 +212,7 @@ public class NRMASessionReplay: NSObject {
         }
         let touches = sessionReplayTouchProcessor.processTouches(touchCapture.touchEvents)
         if clear {
-            sessionReplayTouchCapture.resetEvents()
+            touchCapture.resetEvents()
         }
         return touches
     }
