1b6205f69fdf445a087b7e9e2bcbdd7ceb8d1fef
Michael Bruin <mbruin@newrelic.com>
2025-10-14 16:44:22 -0400

    The reverse does messes up as many things as it solves

--- Agent/SessionReplay/SessionReplayCapture.swift
+++ Agent/SessionReplay/SessionReplayCapture.swift
@@ -54,7 +54,6 @@ class SessionReplayCapture {
             // Check if this is specifically SwiftUI._UIHostingView<SwiftUI.BridgedPresentation.RootView> and reverse subviews if so. This way the captured swiftUI elements are on top of the UIKit ones in playback.
             let className = NSStringFromClass(type(of: currentView))
             if className.contains("_UIHostingView") && className.contains("RootView") {
-                currentParentThingy.subviews.reverse()
                 rootSwiftUIViewID = currentParentThingy.viewDetails.viewId
             }
 
