b726e0c10b22ef404ea8941816f8a9ff6e7ee209
Chris Dillard <cdillard@newrelic.com>
2025-10-15 11:42:46 -0600

    rem: log

--- Agent/SessionReplay/SessionReplayCapture.swift
+++ Agent/SessionReplay/SessionReplayCapture.swift
@@ -151,7 +151,7 @@ class SessionReplayCapture {
          //   logThingys(thingys)
             
             if !thingys.isEmpty {
-                 print("Adding \(thingys) SwiftUI thingys to hosting view thingy")
+                // print("Adding \(thingys) SwiftUI thingys to hosting view thingy")
                 hostingThingy.subviews.append(contentsOf: thingys)
             }
             
