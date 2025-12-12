bde8dd87db6591fcc4ccf48cb51527f0ac32e0b2
Christopher MCAdams <cmcadams@newrelic.com>
2025-12-12 13:56:57 -0500

    fix: Fixes syntax error

--- Test Harness/NRTestApp/NRTestApp/Navigation/MainCoordinator.swift
+++ Test Harness/NRTestApp/NRTestApp/Navigation/MainCoordinator.swift
@@ -11,19 +11,19 @@ import SwiftUI
 // Using the coordinator pattern for navigation
 class MainCoordinator: Coordinator {
     var navigationController: UINavigationController
-    
+
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
-    
+
     // Called at the start of the application
     func start() {
         let vc = ViewControllerProvider.viewController
         vc.coordinator = self
-        
+
         navigationController.pushViewController(vc, animated: false)
     }
-    
+
     func showImageViewController(image: UIImage) {
 #if os(iOS)
         if #available(iOS 14.0, *) {
@@ -33,44 +33,44 @@ class MainCoordinator: Coordinator {
         }
 #endif
     }
-    
+
     func showUtilitiesViewController() {
         let utilitiesViewController = ViewControllerProvider.utilitiesViewController
         navigationController.pushViewController(utilitiesViewController, animated: true)
     }
-    
+
     func showWebViewController() {
 #if os(iOS)
         let webViewController = ViewControllerProvider.webViewController
         navigationController.pushViewController(webViewController, animated: true)
 #endif
     }
-    
+
     func showTextMaskingController() {
         let webViewController = ViewControllerProvider.textMaskingViewController
         navigationController.pushViewController(webViewController, animated: true)
     }
-    
+
     func showCollectionController() {
         let collectionViewController = ViewControllerProvider.collectionViewController
         navigationController.pushViewController(collectionViewController, animated: true)
     }
-    
+
     func showInfiniteScrollController() {
         let infiniteViewController = ViewControllerProvider.infiniteViewController
         navigationController.pushViewController(infiniteViewController, animated: true)
     }
-    
+
     func showInfiniteImageScrollController() {
         let infiniteViewController = ViewControllerProvider.infiniteImageViewController
         navigationController.pushViewController(infiniteViewController, animated: true)
     }
-    
+
     func showDiffTestController() {
         let diffTestViewController = ViewControllerProvider.diffTestViewController
         navigationController.pushViewController(diffTestViewController, animated: true)
     }
-    
+
     func showPerformanceContentView() {
 #if os(iOS)
         if #available(iOS 15.0, *) {
@@ -90,7 +90,7 @@ class MainCoordinator: Coordinator {
         let confidentialViewController = ViewControllerProvider.confidentialViewController
         navigationController.pushViewController(confidentialViewController, animated: true)
     }
-    
+
     func showSwiftUITestView() {
 #if os(iOS)
         if #available(iOS 15.0, *) {
@@ -100,7 +100,7 @@ class MainCoordinator: Coordinator {
         }
 #endif
     }
-    
+
     func showSwiftUIViewRepresentableTestView() {
 #if os(iOS)
         if #available(iOS 15.0, *) {
