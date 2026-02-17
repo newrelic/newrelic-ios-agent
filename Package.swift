74a520f7323e703cb0031b6d1ba187f0631fab62
cdillard-NewRelic <cdillard-NewRelic@users.noreply.github.com>
2026-02-17 17:25:37 +0000

    [7.6.2-rc.1941] Staging Release

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "NewRelic",
     platforms: [
-        .iOS(.v16), .macOS(.v10_14), .tvOS(.v16), .watchOS(.v10)
+        .iOS(.v15), .macOS(.v10_14), .tvOS(.v15), .watchOS(.v10)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -19,8 +19,8 @@ let package = Package(
             name: "NewRelicPackage",
             dependencies: []),
         .binaryTarget(name: "NewRelic",
-                      url: "https://download.newrelic.com/ios_agent/NewRelic_XCFramework_Agent_7.6.1.zip",
-                      checksum: "8d6b95c21cfc5c2d6c7842afb66ff3083aafe919067e4d77dc92ed52a3e018ec")
+                      url: "https://download.newrelic.com/ios-v5/NewRelic_XCFramework_Agent_7.6.2-rc.1941.zip",
+                      checksum: "10fc933239d67ea58963b7b67f05cc60b17566e1699b8c64e3d71552a37e650f")
     ]
 )
 
