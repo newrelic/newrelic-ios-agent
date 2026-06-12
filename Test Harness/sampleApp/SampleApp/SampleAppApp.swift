8a13b6c0b261381a46ea71d24238257243fb1e5a
Chris Dillard <cdillard@newrelic.com>
2026-06-12 17:41:04 -0600

    add nr start in SampleAppApp

--- Test Harness/sampleApp/SampleApp/SampleAppApp.swift
+++ Test Harness/sampleApp/SampleApp/SampleAppApp.swift
@@ -1,8 +1,16 @@
 import SwiftUI
+import NewRelic
 
 @main
 struct SampleAppApp: App {
     @StateObject private var appState = AppState()
+    
+    init() {
+        // TODO: Replace with a real New Relic mobile application token, and enable
+        // Session Replay for this app in the New Relic UI. Masking is wired below
+        // via NRConditionalMaskView regardless, but no replay is captured until then.
+        NewRelic.start(withApplicationToken: "NR_APP_TOKEN_PLACEHOLDER")
+    }
 
     var body: some Scene {
         WindowGroup {
